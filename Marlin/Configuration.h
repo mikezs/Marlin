2eb99491e0880f27dbd2b0f9a10080deb1f7d368
Mike Bignell <mike@mbignell.com>
2024-01-30 10:45:29 +0000

    Max feedrate after tests

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1249,7 +1249,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 25, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 400, 400, 25, 25 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
